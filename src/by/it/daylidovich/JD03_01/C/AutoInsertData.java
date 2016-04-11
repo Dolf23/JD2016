cfc8d2347a74cbc884fb198fc0c226b8b543dbb6
Dolf23 <dmitry.dailidovich@gmail.com>
2016-04-11 12:57:31 +0300

    JD003_01 update connection.

--- src/by/it/daylidovich/JD03_01/C/AutoInsertData.java
+++ src/by/it/daylidovich/JD03_01/C/AutoInsertData.java
@@ -1,13 +1,12 @@
 package by.it.daylidovich.JD03_01.C;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class AutoInsertData {
     public static void main(String[] args) {
-        try(Connection connection  = DriverManager.getConnection(CN.URL_DB, CN.USER_DB, CN.PASSWORD_DB);
+        try(Connection connection  =  CN.getConnection();
             Statement statement = connection.createStatement()) {
 
             //добавление записей в таблицы
