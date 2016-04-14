c00ba55ba795e4f49e376da08f74189bcd24db5b
Dolf <dmitry.dailidovich@gmail.com>
2016-04-14 15:16:16 +0300

    Travel agency added servlets

---
+++ src/projects/daylidovich/TravelAgency/servlets/DAO/RoleDAO.java
@@ -0,0 +1,23 @@
+package projects.daylidovich.TravelAgency.servlets.DAO;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class RoleDAO {
+
+    public static Integer getID(String role) throws SQLException {
+        Integer out = null;
+        String sql = String.format("SELECT ID FROM role_d WHERE Role='%s';", role);
+        ResultSet resultSet = by.it.daylidovich.JD03_03.DAO.DAO.getStatement().executeQuery(sql);
+        if (resultSet.next()) out = resultSet.getInt("id");
+        return out;
+    }
+
+    public static String getRole(Integer id) throws SQLException {
+        String out = null;
+        String sql = String.format("SELECT * FROM role_d WHERE ID='%d';", id);
+        ResultSet resultSet = DAO.getStatement().executeQuery(sql);
+        if (resultSet.next()) out = resultSet.getString("Role");
+        return out;
+    }
+}
--- src/projects/daylidovich/TravelAgency/servlets/Reserv.java
+++
@@ -1,4 +0,0 @@
-package projects.daylidovich.TravelAgency.servlets;
-
-public class Reserv {
-}
