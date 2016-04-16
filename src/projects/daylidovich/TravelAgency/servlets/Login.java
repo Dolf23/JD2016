987dddfc7ff587ddee208b5d2e6999fe69f812bb
Dmitry <dmitry.dailidovich@gmail.com>
2016-04-16 17:59:56 +0300

    filter start

--- src/projects/daylidovich/TravelAgency/servlets/Login.java
+++ src/projects/daylidovich/TravelAgency/servlets/Login.java
@@ -1,4 +1,32 @@
 package projects.daylidovich.TravelAgency.servlets;
 
-public class Login {
+import projects.daylidovich.TravelAgency.servlets.DAO.UsersDAO;
+import projects.daylidovich.TravelAgency.servlets.DTO.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+public class Login extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("Login");
+        String password = req.getParameter("passwordinput");
+
+        UsersDAO usersDAO = new UsersDAO();
+        List<User> list = usersDAO.getAll("WHERE Login='" + login + "', Password='" + password + "'");
+        if (0==list.size())
+            resp.sendRedirect("login.jsp");
+        else{
+            HttpSession session = req.getSession();
+            session.setAttribute("Login", login);
+            session.setAttribute("UserIn", true);
+            resp.sendRedirect("tourSelect.jsp");
+        }
+
+    }
 }
